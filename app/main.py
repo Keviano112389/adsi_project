d8ce7861494184db12bfcf67189e8fb13c5b52a0
Keviano112389 <kevin.shao@vgw.co>
2021-03-16 23:29:59 +1100

    heroku

--- app/main.py
+++ app/main.py
@@ -6,9 +6,309 @@ from fastapi.encoders import jsonable_encoder
 from torch.utils.data import DataLoader
 import torch
 from typing import List, Optional
-from pytorch import get_device, split_sets_random, pop_target
+from pytorch import get_device,split_sets_random,PytorchDataset,pop_target,PytorchMultiClass,train_classification,pop_target,predict
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+import numpy as np
 
 app = FastAPI()
+
+preprocess = load('../models/preprocess.joblib')
+
+model = torch.load('../models/beer_review.pt')
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+device = get_device()
+
+EPOCHS = 1
+BATCH_SIZE = 16
+
+def get_device():
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu') # don't have GPU 
+    return device
+
+    
+
+class PytorchMultiClass(nn.Module):
+    def __init__(self):
+        super(PytorchMultiClass, self).__init__()
+        
+        self.layer_1 = nn.Linear(6, 256)
+        self.layer_2 = nn.Linear(256, 64)
+        self.layer_3 = nn.Linear(64, 32)
+        self.layer_out = nn.Linear(32, 104) 
+        
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.2)
+        self.batchnorm1 = nn.BatchNorm1d(256)
+        self.batchnorm2 = nn.BatchNorm1d(64)
+        self.batchnorm3 = nn.BatchNorm1d(32)
+        
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = self.batchnorm1(x)
+        x = self.relu(x)
+        
+        x = self.layer_2(x)
+        x = self.batchnorm2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        
+        x = self.layer_3(x)
+        x = self.batchnorm3(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        
+        x = self.layer_out(x)
+        
+        return x
+        self.softmax = nn.Softmax(dim=1)
+
+def train_classification(train_data, model, criterion, optimizer, batch_size, device, scheduler=None, generate_batch=None):
+    """Train a Pytorch multi-class classification model
+
+    Parameters
+    ----------
+    train_data : torch.utils.data.Dataset
+        Pytorch dataset
+    model: torch.nn.Module
+        Pytorch Model
+    criterion: function
+        Loss function
+    optimizer: torch.optim
+        Optimizer
+    bacth_size : int
+        Number of observations per batch
+    device : str
+        Name of the device used for the model
+    scheduler : torch.optim.lr_scheduler
+        Pytorch Scheduler used for updating learning rate
+    collate_fn : function
+        Function defining required pre-processing steps
+
+    Returns
+    -------
+    Float
+        Loss score
+    Float:
+        Accuracy Score
+    """
+    
+    # Set model to training mode
+    model.train()
+    train_loss = 0
+    train_acc = 0
+    
+    # Create data loader
+    data = DataLoader(train_data, batch_size=batch_size, shuffle=True, collate_fn=generate_batch)
+    
+    # Iterate through data by batch of observations
+    for feature, target_class in data:
+
+        # Reset gradients
+        optimizer.zero_grad()
+        
+        # Load data to specified device
+        feature, target_class = feature.to(device), target_class.to(device)
+        
+        # Make predictions
+        output = model(feature)
+        
+        # Calculate loss for given batch
+        loss = criterion(output, target_class.long())
+
+        # Calculate global loss
+        train_loss += loss.item()
+        
+        # Calculate gradients
+        loss.backward()
+
+        # Update Weights
+        optimizer.step()
+        
+        # Calculate global accuracy
+        train_acc += (output.argmax(1) == target_class).sum().item()
+
+    # Adjust the learning rate
+    if scheduler:
+        scheduler.step()
+
+    return train_loss / len(train_data), train_acc / len(train_data)
+
+def test_regression(test_data, model, criterion, batch_size, device, collate_fn=None):
+    """Calculate performance of a Pytorch regresssion model
+
+    Parameters
+    ----------
+    test_data : torch.utils.data.Dataset
+        Pytorch dataset
+    model: torch.nn.Module
+        Pytorch Model
+    criterion: function
+        Loss function
+    bacth_size : int
+        Number of observations per batch
+    device : str
+        Name of the device used for the model
+    collate_fn : function
+        Function defining required pre-processing steps
+
+    Returns
+    -------
+    Float
+        Loss score
+    Float:
+        RMSE Score
+    """    
+    
+    # Set model to evaluation mode
+    model.eval()
+    test_loss = 0
+
+    # Create data loader
+    data = DataLoader(test_data, batch_size=batch_size, collate_fn=collate_fn)
+    
+    # Iterate through data by batch of observations
+    for feature, target_class in data:
+        
+        # Load data to specified device
+        feature, target_class = feature.to(device), target_class.to(device)
+        
+        # Set no update to gradients
+        with torch.no_grad():
+            
+            # Make predictions
+            output = model(feature)
+            
+            # Calculate loss for given batch
+            loss = criterion(output, target_class)
+            
+            # Calculate global loss
+            test_loss += loss.item()
+            
+    return test_loss / len(test_data), np.sqrt(test_loss / len(test_data))
+
+def split_sets_random(df, target_col, test_ratio=0.2, to_numpy=False):
+    """Split sets randomly
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe
+    target_col : str
+        Name of the target column
+    test_ratio : float
+        Ratio used for the validation and testing sets (default: 0.2)
+
+    Returns
+    -------
+    Numpy Array
+        Features for the training set
+    Numpy Array
+        Target for the training set
+    Numpy Array
+        Features for the validation set
+    Numpy Array
+        Target for the validation set
+    Numpy Array
+        Features for the testing set
+    Numpy Array
+        Target for the testing set
+    """
+    
+    from sklearn.model_selection import train_test_split
+    
+    features, target = pop_target(df=df, target_col=target_col, to_numpy=to_numpy)
+    
+    X_data, X_test, y_data, y_test = train_test_split(features, target, test_size=test_ratio, random_state=8)
+    
+    val_ratio = test_ratio / (1 - test_ratio)
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=val_ratio, random_state=8)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def pop_target(df, target_col, to_numpy=False):
+    """Extract target variable from dataframe and convert to nympy arrays if required
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe
+    target_col : str
+        Name of the target variable
+    to_numpy : bool
+        Flag stating to convert to numpy array or not
+
+    Returns
+    -------
+    pd.DataFrame/Numpy array
+        Subsetted Pandas dataframe containing all features
+    pd.DataFrame/Numpy array
+        Subsetted Pandas dataframe containing the target
+    """
+
+    df_copy = df.copy()
+    target = df_copy.pop(target_col)
+    
+    if to_numpy:
+        df_copy = df_copy.to_numpy()
+        target = target.to_numpy()
+    
+    return df_copy, target
+
+def test_function(test_data, model, criterion, batch_size, device, collate_fn=None):
+    """Calculate performance of a Pytorch regresssion model
+
+    Parameters
+    ----------
+    test_data : torch.utils.data.Dataset
+        Pytorch dataset
+    model: torch.nn.Module
+        Pytorch Model
+    criterion: function
+        Loss function
+    bacth_size : int
+        Number of observations per batch
+    device : str
+        Name of the device used for the model
+    collate_fn : function
+        Function defining required pre-processing steps
+
+    Returns
+    -------
+    Float
+        Loss score
+    Float:
+        RMSE Score
+    """    
+    
+    # Set model to evaluation mode
+    model.eval()
+    test_loss = 0
+
+    # Create data loader
+    data = DataLoader(test_data, batch_size=batch_size, collate_fn=collate_fn)
+    
+    # Iterate through data by batch of observations
+    for feature, target_class in data:
+        
+        # Load data to specified device
+        feature, target_class = feature.to(device), target_class.to(device)
+        
+        # Set no update to gradients
+        with torch.no_grad():
+            
+            # Make predictions
+            output = model(feature)
+                     
+    return output
 
 
 @app.get("/")
@@ -23,19 +323,19 @@ def format_features(brewery_name: str,	review_overall: int, review_aroma: int,
                     review_appearance: int, review_palate:int, review_taste:int):
   return {
         'brewery_name': [brewery_name],
-        'review_overall': [overall],
-        'review_aroma': [aroma],
-        'review_appearance': [appearance],
-        'review_palate': [palate],
-        'review_taste': [taste]
+        'review_overall': [review_overall],
+        'review_aroma': [review_aroma],
+        'review_appearance': [review_appearance],
+        'review_palate': [review_palate],
+        'review_taste': [review_taste]
     }
 
 @app.post("/beers/type/")
 def predict(brewery_name: str,	review_overall: int, review_aroma: int,
             review_appearance: int, review_palate:int, review_taste:int):
-    features = format_features(brewery_name,overall,aroma,appearance,palate,taste)
+    features = format_features(brewery_name,review_overall,review_aroma,review_appearance,review_palate,review_taste)
     obs = pd.DataFrame(features)
-    pred = nn_pipe.predict(obs)
+    pred = test_function(obs,model=model, criterion=criterion, batch_size=BATCH_SIZE, device=device)
     return JSONResponse(pred.tolist())
 
 @app.get("/model/architecture/")
--- app/pytorch.py
+++ app/pytorch.py
@@ -286,5 +286,57 @@ def pop_target(df, target_col, to_numpy=False):
     
     return df_copy, target
 
+def predict(test_data, model, criterion, batch_size, device, collate_fn=None):
+    """Calculate performance of a Pytorch regresssion model
+
+    Parameters
+    ----------
+    test_data : torch.utils.data.Dataset
+        Pytorch dataset
+    model: torch.nn.Module
+        Pytorch Model
+    criterion: function
+        Loss function
+    bacth_size : int
+        Number of observations per batch
+    device : str
+        Name of the device used for the model
+    collate_fn : function
+        Function defining required pre-processing steps
+
+    Returns
+    -------
+    Float
+        Loss score
+    Float:
+        RMSE Score
+    """    
+    
+    # Set model to evaluation mode
+    model.eval()
+    test_loss = 0
+
+    # Create data loader
+    data = DataLoader(test_data, batch_size=batch_size, collate_fn=collate_fn)
+    
+    # Iterate through data by batch of observations
+    for feature, target_class in data:
+        
+        # Load data to specified device
+        feature, target_class = feature.to(device), target_class.to(device)
+        
+        # Set no update to gradients
+        with torch.no_grad():
+            
+            # Make predictions
+            output = model(feature)
+            
+            # Calculate loss for given batch
+            loss = criterion(output, target_class)
+            
+            # Calculate global loss
+            test_loss += loss.item()
+            
+    return test_loss / len(test_data), np.sqrt(test_loss / len(test_data))
 
 
